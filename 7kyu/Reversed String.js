621f9ba115d09469134a1b1eeb42a91c6b9ffb44
Erik Golden <erikgolden7@gmail.com>
2018-02-15 11:27:12 -0600

    Reversed String

---
+++ 7kyu/Reversed String.js
@@ -0,0 +1,7 @@
+// Complete the solution so that it reverses the string value passed into it.
+
+// solution('world'); // returns 'dlrow'
+
+// Solution:
+
+solution = str => str.split('').reverse().join('')
